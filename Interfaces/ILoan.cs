f81a391225b83f513ddd64e30a658bdd33c8fd7d
vladimir <vstrukoff@yandex.ru>
2025-01-12 00:00:05 +0300

    Revert "Implement bank customer"
    This reverts commit a424957b3c53af44a3a9e5285819971d3075485e.

--- Interfaces/ILoan.cs
+++
@@ -1,9 +0,0 @@
-namespace cs_bank_customer.Interfaces;
-
-public interface ILoan
-{
-    public decimal GetLoanAmount();
-    public string GetLoanDescription();
-    public bool IsExpired();
-    public decimal GetMonthlyIncome();
-}
